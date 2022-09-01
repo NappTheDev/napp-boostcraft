b1e1e48122af4e2e19655bf81cb9c01dc14996d4
Napp <36120206+NappTheDev@users.noreply.github.com>
2022-09-01 17:16:39 -0400

    config

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config=Config or {}
+
+Config.BoostingLaptopDevice = "boostinglaptop"
+Config.BoostingDevice = "boostingdisabler"
+
+Config.LaptopRecipe = {
+    ['laptop'] = 1,
+    ['vpn'] = 1,
+}
+
+Config.BoostRecipe = {
+    ['laptop'] = 1,
+    ['electronickit'] = 1,
+    ['radio'] = 1,
+}
+
